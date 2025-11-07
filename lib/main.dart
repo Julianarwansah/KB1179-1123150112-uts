98afd99e78e657c30a62cb4ecc241cbdfa5f4b78
NamaAnda <julianarwansahh@gmail.com>
2025-11-07 20:19:00 +0700

    menambahkan scaffold dan appbar

--- lib/main.dart
+++ lib/main.dart
@@ -18,11 +18,34 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
       ),
       debugShowCheckedModeBanner: false,
-      home: const Scaffold(
-        body: Center(
-          child: Text('Inisialisasi Aplikasi Tema Flutter...'),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Aplikasi Tema Flutter'),
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(20),
+          child: Padding(
+            padding: const EdgeInsets.only(bottom: 8.0),
+            child: Text(
+              'NIM: 1123150112',
+              style: const TextStyle(
+                color: Colors.white70,
+                fontSize: 12,
+              ),
+            ),
+          ),
         ),
       ),
+      body: const Center(child: Text('Bagian 2: AppBar selesai')),
     );
   }
 }
